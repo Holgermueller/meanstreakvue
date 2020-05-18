9bdd3ea042d590900b3427bafe12c5fabdee035e
Holger Mueller <9jholger@gmail.com>
2020-05-18 11:41:55 -0500

    most single previous post data on dom

--- pages/previous/_singlePostId/index.vue
+++ pages/previous/_singlePostId/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>A single post will appear here.</div>
+  <div class="post">
+    <v-card>
+      <v-card-title>
+        <h1>
+          {{ title }}
+        </h1>
+      </v-card-title>
+      <v-card-subtitle>
+        {{ dateTime }}
+      </v-card-subtitle>
+      <v-card-text></v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <h6>
+          {{ authorArtist }}
+        </h6>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {}
+const moment = require('moment')
+import { createClient } from '~/plugins/contentful'
+const contentfulClient = createClient()
+
+export default {
+  asyncData({ data, params }) {
+    return Promise.all([contentfulClient.getEntry(params.singlePostId)])
+      .then(([page]) => {
+        return {
+          title: page.fields.title,
+          authorArtist: page.fields.authorArtist,
+          dateTime: page.fields.dateTime
+        }
+      })
+      .catch(console.error)
+  }
+}
 </script>
 
 <style scoped></style>
