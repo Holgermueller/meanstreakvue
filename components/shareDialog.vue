8c2cea86d08867eb1e4939d3533f8bd776ac0481
Holger Mueller <9jholger@gmail.com>
2020-06-02 14:54:17 -0500

    colored share buttons

--- components/shareDialog.vue
+++ components/shareDialog.vue
@@ -18,7 +18,7 @@
             :title="sharing.title"
             :hashtags="sharing.hashtags"
           >
-            <v-btn icon :class="network.class">
+            <v-btn icon :color="network.color">
               <v-icon>{{ network.class }}</v-icon>
             </v-btn>
           </ShareNetwork>
@@ -34,45 +34,71 @@ export default {
     return {
       dialog: false,
       sharing: {
-        url: 'url',
+        url: '#',
         title: 'Check out the newest cartoon from MeanStreak!',
+        description: "It's MeanStreak's newest cartoon to underwhelm you.",
         hashtags: 'MeanStreak'
       },
       networks: [
         {
           network: 'Twitter',
           name: 'Twitter',
-          class: 'mdi-twitter'
+          class: 'mdi-twitter',
+          color: '#00acee'
         },
         {
           network: 'Reddit',
           name: 'Reddit',
-          class: 'mdi-reddit'
+          class: 'mdi-reddit',
+          color: '#ff4602'
+        },
+        {
+          network: 'telegram',
+          name: 'Telegram',
+          class: 'mdi-telegram',
+          color: '#0088cc'
         },
         {
           network: 'whatsapp',
           name: 'whatsapp',
-          class: 'mdi-whatsapp'
+          class: 'mdi-whatsapp',
+          color: '#45eb63'
         },
         {
           network: 'skype',
           name: 'skype',
-          class: 'mdi-skype'
+          class: 'mdi-skype',
+          color: '#12aef2'
+        },
+        {
+          network: 'email',
+          name: 'Email',
+          class: 'mdi-email',
+          color: '#333333'
         },
         {
           network: 'pinterest',
           name: 'Pinterest',
-          class: 'mdi-pinterest'
+          class: 'mdi-pinterest',
+          color: '#e6001f'
         },
         {
           network: 'linkedin',
           name: 'Linkedin',
-          class: 'mdi-linkedin'
+          class: 'mdi-linkedin',
+          color: '#067bb6'
+        },
+        {
+          network: 'sms',
+          name: 'sms',
+          class: 'mdi-message-reply-text',
+          color: '#333333'
         },
         {
           network: 'facebook',
           name: 'facebook',
-          class: 'mdi-facebook'
+          class: 'mdi-facebook',
+          color: '#4967aa'
         }
       ]
     }
